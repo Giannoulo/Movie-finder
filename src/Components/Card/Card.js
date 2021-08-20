bd9402d33d45654a40a53e2031dd05f9a390b8ef
Giannoulo <ggiannoulopoulos@konnektable.com>
2021-08-20 14:22:52 +0300

    Fixed card row height

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,7 +2,7 @@
 function Card() {
     return (
         <div className="row justify-content-center" id="card-row">
-            <div className="col" id="card-col">Card col</div>
+            <div className="col" id="card-col"><h1>Suck it Manos</h1></div>
         </div>
     )
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,8 @@ import { faFilm } from '@fortawesome/free-solid-svg-icons'
 function Header() {
     return (
         <div className="row align-items-center" id="header-row">
-            <div className="col" id="header-col"><FontAwesomeIcon icon={faFilm} id="header-icon"/>Movie Finder</div>
+            <div className="col" id="header-col">
+                <FontAwesomeIcon icon={faFilm} id="header-icon"/>Movie Finder</div>
         </div>
     )
 }
